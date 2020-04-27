b46b6bf98f371d5c3eb7294a28bcefa70c17b477
Benjamin Stürmer <webmaster@stuermer-benjamin.de>
2020-04-27 10:10:42 +0200

    change ndk path

--- android.zsh
+++ android.zsh
@@ -8,10 +8,19 @@ alias kill_gradle_cache='find ~/.gradle -type f -name "*.lock" | while read f; d
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export ANDROID_AVD_HOME=$HOME/.android/avd
 export ANDROID_SDK_HOME=$ANDROID_HOME
-export ANDROID_NDK_HOME=ANDROID_HOME/ndk-bundle/
+export NDK=$ANDROID_SDK_HOME/ndk/20.0.5594570/
 export GRADLE_USER_HOME=$HOME/.gradle
 
-export PATH=${PATH}:$ANDROID_NDK_HOME
+export HOST_TAG=darwin-x86_64
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_TAG
+export AR=$TOOLCHAIN/bin/aarch64-linux-android-ar
+export AS=$TOOLCHAIN/bin/aarch64-linux-android-as
+export CC=$TOOLCHAIN/bin/aarch64-linux-android21-clang
+export CXX=$TOOLCHAIN/bin/aarch64-linux-android21-clang++
+export LD=$TOOLCHAIN/bin/aarch64-linux-android-ld
+export RANLIB=$TOOLCHAIN/bin/aarch64-linux-android-ranlib
+export STRIP=$TOOLCHAIN/bin/aarch64-linux-android-strip
+
 export PATH=${PATH}:$ANDROID_HOME/tools
 export PATH=${PATH}:$ANDROID_HOME/platform-tools
 export PATH=${PATH}:$ANDROID_HOME/build-tools/28.0.2
